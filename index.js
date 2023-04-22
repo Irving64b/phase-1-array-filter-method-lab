7521e47d617cd106728a4cd741ee77e4ee5ea341
Irving Medina <irving.64@hotmail.com>
2023-04-22 15:40:52 -0400

    complete filter

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(function (driver) {return driver.toUpperCase() === string.toUpperCase()})
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver) {return driver.toUpperCase().slice(0, string.length) === string.toUpperCase()})
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (driver) {return driver.name === string})
+}
